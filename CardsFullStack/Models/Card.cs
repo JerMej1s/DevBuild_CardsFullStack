12870eb102c790e100dd77e5960a5e63f40553dd
Jeremy Jones <JeremyJones@QuickenLoans.com>
2021-11-03 13:14:16 -0400

    Added models

---
+++ CardsFullStack/Models/Card.cs
@@ -0,0 +1,20 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CardsFullStack.Models
+{
+    [Table("card")]
+    public class Card
+    {
+        [Key]
+        public int id { get; set; }
+        public string deck_id { get; set; }
+        public string image { get; set; }
+        public string card_code { get; set; }
+        public string username { get; set; }
+        public DateTime created_at { get; set; }
+    }
+}
---
+++ CardsFullStack/model.sql
@@ -0,0 +1,21 @@
+﻿create database CardGame;
+
+use CardGame;
+
+create table Deck (
+    deck_id varchar(20),
+    is_current boolean,
+    username varchar(20),
+    created_at datetime,
+    primary key (deck_id)
+);
+
+create table Card (
+	id int not null auto_increment,
+    deck_id varchar(20),
+    image varchar(200),
+    card_code char(2),
+    username varchar(20),
+    created_at datetime,
+    primary key (id)
+);
